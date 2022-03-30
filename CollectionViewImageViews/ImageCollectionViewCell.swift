3d09c094909a630c959a23c86766f8f869c749e7
Michael Miles <michael.miles@seasoned.co>
2022-03-30 15:53:35 -0400

    created collection view with images

---
+++ CollectionViewImageViews/ImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.swift
+//  CollectionViewImageViews
+//
+//  Created by Michael Miles on 3/30/22.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
